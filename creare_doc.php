2ee1fd9a168fc66a1ac09e3465c9ddc1b8924b30
secret <522184309@qq.com>
2019-04-29 14:35:43 +0800

    分支测试

--- creare_doc.php
+++ creare_doc.php
@@ -2,34 +2,37 @@
 class word 
 {  
 
+	function test()
+	{
+		echo 'hello world';
+	}
+	function start() 
+	{ 
+	ob_start(); 
+	print'<html xmlns:o="urn:schemas-microsoft-com:office:office" 
+	xmlns:w="urn:schemas-microsoft-com:office:word" 
+	xmlns="http://www.w3.org/TR/REC-html40">'; 
 
-function start() 
-{ 
-ob_start(); 
-print'<html xmlns:o="urn:schemas-microsoft-com:office:office" 
-xmlns:w="urn:schemas-microsoft-com:office:word" 
-xmlns="http://www.w3.org/TR/REC-html40">'; 
+	} 
 
-} 
+	function save($path) 
+	{ 
 
-function save($path) 
-{ 
+	print "</html>"; 
+	$data = ob_get_contents(); 
 
-print "</html>"; 
-$data = ob_get_contents(); 
+	ob_end_clean(); 
 
-ob_end_clean(); 
+	$this->wirtefile ($path,$data); 
+	} 
 
-$this->wirtefile ($path,$data); 
-} 
+	function wirtefile ($fn,$data) 
+	{ 
 
-function wirtefile ($fn,$data) 
-{ 
-
-$fp=fopen($fn,"wb"); 
-fwrite($fp,$data); 
-fclose($fp); 
-} 
+	$fp=fopen($fn,"wb"); 
+	fwrite($fp,$data); 
+	fclose($fp); 
+	} 
 
 } 
 
